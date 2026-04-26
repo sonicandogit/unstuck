bf5f0386e6467ec9b73f96329ba6d6bb2394afbf
sonicandogit <lucassanchezsampedro@gmail.com>
2026-04-26 10:45:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,30 @@
-// Unstuck Service Worker
-const CACHE = 'unstuck-v1';
+// Unstuck Service Worker v2
+const CACHE = 'unstuck-v2';
 
 self.addEventListener('install', e => {
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(['/unstuck/index.html', '/unstuck/unstuckfavicon.png']).catch(()=>{}))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
