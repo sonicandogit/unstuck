99d820cbe7fa6c00615c3c7227a86eb3975ff134
sonicandogit <lucassanchezsampedro@gmail.com>
2026-04-26 10:37:51 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+// Unstuck Service Worker
+const CACHE = 'unstuck-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
